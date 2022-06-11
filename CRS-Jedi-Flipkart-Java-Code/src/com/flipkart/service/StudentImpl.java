ed44d551d4445309cdf3d4758187ddb3ccc88b0c
Het <hetfadia@gmail.com>
2022-06-12 00:54:47 +0530

    Update StudentImpl.java

--- CRS-Jedi-Flipkart-Java-Code/src/com/flipkart/service/StudentImpl.java
+++ CRS-Jedi-Flipkart-Java-Code/src/com/flipkart/service/StudentImpl.java
@@ -1,4 +1,12 @@
 package com.flipkart.service;
 
-public class StudentImpl {
+public class StudentImpl extends UserImpl{
+    public void addCourse(int studentId,int courseid){
+
+    }
+
+    public void dropCourse(int studentid, int courseid){
+
+    }
+
 }

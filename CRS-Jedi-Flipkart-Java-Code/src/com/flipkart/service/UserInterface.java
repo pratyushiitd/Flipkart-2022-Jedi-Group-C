bc152b24729818fb98c4494756198b796852a172
riddhijain4 <riddhi039btcse19@igdtuw.in>
2022-06-12 00:49:56 +0530

    user_interface

--- CRS-Jedi-Flipkart-Java-Code/src/com/flipkart/service/UserInterface.java
+++ CRS-Jedi-Flipkart-Java-Code/src/com/flipkart/service/UserInterface.java
@@ -1,5 +1,8 @@
 package com.flipkart.service;
+import java.util.*;
 
 public interface UserInterface {
+    boolean resetPassword(String userID, String newPassword);
+    public boolean login(String userID,String password);
 
 }

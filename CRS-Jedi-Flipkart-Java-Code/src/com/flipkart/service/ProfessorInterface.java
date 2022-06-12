eccbcc9770644f76412f222ed2f39095b3307395
Pratyush Saini <66582958+pratyushiitd@users.noreply.github.com>
2022-06-12 23:43:11 +0530

    professor service updated

--- CRS-Jedi-Flipkart-Java-Code/src/com/flipkart/service/ProfessorInterface.java
+++ CRS-Jedi-Flipkart-Java-Code/src/com/flipkart/service/ProfessorInterface.java
@@ -6,5 +6,11 @@ public interface ProfessorInterface {
     
     public List<Student> getStudentsList(int courseID);
     public void submitGrades(int courseID, int studentID, int grade);
-    
+    public void selectCoursestoTeach(int professorID, int courseID);
+    public void dropCoursestoTeach(int professorID, int courseID);
+    public void viewCourses(int professorID);
+    public void viewResults(int professorID);
+    public void viewSchedule(int professorID);
+    public void viewProfile(int professorID);
+
 }

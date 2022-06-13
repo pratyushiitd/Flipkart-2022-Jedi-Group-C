18bae4d1f08ddff636c903c4d132cfe67cfe150d
Sayani Mallick <sayani.mallick@BLRETV-X039G8WN.local>
2022-06-13 11:54:58 +0530

    Updated RegisteredStudent

--- CRS-Jedi-Flipkart-Java-Code/src/com/flipkart/bean/RegisteredStudent.java
+++ CRS-Jedi-Flipkart-Java-Code/src/com/flipkart/bean/RegisteredStudent.java
@@ -1,14 +1,21 @@
 package com.flipkart.bean;
+import java.util.HashMap;
 import java.util.List;
+import com.flipkart.bean.Student;
 
 public class RegisteredStudent extends User {
-    List<Student> registeredStudents;
 
-    public List<Student> getRegisteredStudents() {
+    HashMap<Integer, List<Student>> registeredStudents;
+
+    public List<Student> getRegisteredStudentList(int semester){
+        return registeredStudents.get(semester);
+    }
+
+    public HashMap<Integer, List<Student>> getRegisteredStudents() {
         return registeredStudents;
     }
 
-    public void setRegisteredStudents(List<Student> registeredStudents) {
+    public void setRegisteredStudents(HashMap<Integer, List<Student>> registeredStudents) {
         this.registeredStudents = registeredStudents;
     }
 }

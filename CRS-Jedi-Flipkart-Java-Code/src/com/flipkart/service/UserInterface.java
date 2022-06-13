0442a57ab0a8b5b7b54e6d82680c282a9afe0fb3
Het <62541263+HETFADIA@users.noreply.github.com>
2022-06-13 11:59:03 +0530

    user implementation (service) done

--- CRS-Jedi-Flipkart-Java-Code/src/com/flipkart/service/UserInterface.java
+++ CRS-Jedi-Flipkart-Java-Code/src/com/flipkart/service/UserInterface.java
@@ -4,10 +4,10 @@ import java.util.*;
 public interface UserInterface {
     boolean resetPassword(String userID, String newPassword);
     public boolean login(String userID,String password);
-    public boolean register(String name, String role, int userID, String password, long mobile, String email_id);
-    public boolean setname(int userID);
-    public boolean setrole(int userID);
-    public boolean setmobile(int userID);
-    public boolean setemail(int userID);
-    public boolean setpassword(int userID);
+    public boolean register(String name, String role, String userID, String password, long mobile, String email_id);
+    public boolean setname(String name);
+    public boolean setrole(String role);
+    public boolean setmobile(long mobile);
+    public boolean setemail(string email_id);
+    public boolean setpassword(string password);
 }

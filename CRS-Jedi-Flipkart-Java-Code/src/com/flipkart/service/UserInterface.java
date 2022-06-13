7346f46d76ab44b27255ae3c37fdb166a7fbcdc5
Het <hetfadia@gmail.com>
2022-06-13 13:01:57 +0530

    some errors corrected

--- CRS-Jedi-Flipkart-Java-Code/src/com/flipkart/service/UserInterface.java
+++ CRS-Jedi-Flipkart-Java-Code/src/com/flipkart/service/UserInterface.java
@@ -5,9 +5,9 @@ public interface UserInterface {
     boolean resetPassword(String userID, String newPassword);
     public boolean login(String userID,String password);
     public boolean register(String name, String role, String userID, String password, long mobile, String email_id);
-    public boolean setname(String name);
-    public boolean setrole(String role);
-    public boolean setmobile(long mobile);
-    public boolean setemail(String email_id);
-    public boolean setpassword(String password);
+    public boolean setname(String userId, String name);
+    public boolean setrole(String userId, String role);
+    public boolean setmobile(String userId, long mobile);
+    public boolean setemail(String userId, String email_id);
+    public boolean setpassword(String userId, String password);
 }

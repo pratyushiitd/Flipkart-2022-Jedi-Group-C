4a2e553c3cf7276923dfe263e06523ddf5a76f31
Sayani Mallick <sayani.mallick@BLRETV-X039G8WN.local>
2022-06-16 18:03:15 +0530

    Added exceptions and AdminDAO

--- CRS-Jedi-Flipkart-Java-Code/src/com/flipkart/dao/AdminDAO.java
+++ CRS-Jedi-Flipkart-Java-Code/src/com/flipkart/dao/AdminDAO.java
@@ -1,4 +1,35 @@
 package com.flipkart.dao;
 
+import java.sql.SQLException;
+import java.util.List;
+
+import com.flipkart.bean.Professor;
+import com.flipkart.bean.Student;
+import com.flipkart.exception.*;
+import com.flipkart.bean.Course;
+
+
 public interface AdminDAO {
+
+    List<Course> viewCourses();
+    String addCourse(Course newCourse);
+
+    void dropCourse(String courseId) throws CourseNotFoundException;
+
+    List<Student> getPendingStudents();
+
+    String approveStudent(Student newStudent);
+
+
+    String addProfessor(Professor newProfessor) throws EmailAlreadyInUseException;
+
+    List<Professor> viewProfessorList();
+
+    void dropProfessor(String professorId) throws UserNotFoundException;
+
+    List<Student> getPendingGradeStudents();
+
+
+    void generateGradeCard(String studentId, String semester) throws GradeNotAllottedException;
+
 }
